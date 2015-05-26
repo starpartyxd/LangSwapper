ef5965bfb7d020c7760d1fec7c5d5334f5700588
Omega2058 <tshomega@gmail.com>
2015-05-26 02:50:08 -0400

    Bug fixes and check for system language implemented

--- src/crt0.c
+++ src/crt0.c
@@ -19,25 +19,30 @@
 
 #include <pspkernel.h>
 #include <pspthreadman.h>
+#include <psputility.h>
+#include <psputility_sysparam.h>
 #include <pspctrl.h>
 
 // Defines for sceImposeSetLanguageMode.
-#define ASM_RANGE_MAX 				0x84
-#define ASM_LANGUAGE_INSTRUCTION	0xAD84033C		// sw $a0, 828($t4)
-#define ASM_BUTTON_INSTRUCTION		0xAD850340		// sw $a1, 832($t4)
-#define ASM_PATCHED_INSTRUCTION		0xAD8D0340		// sw $t5, 832($t4)
+#define ASM_RANGE_MAX 							0x84
+#define ASM_LANGUAGE_INSTRUCTION				0xAD84033C		// sw $a0, 828($t4)
+#define ASM_BUTTON_INSTRUCTION					0xAD850340		// sw $a1, 832($t4)
+#define ASM_PATCHED_INSTRUCTION					0xAD8D0340		// sw $t5, 832($t4)
 
 // Defines for sceUtilitySavedataInitStart.
-#define InitStart_OFFSET			0x18
+#define InitStart_OFFSET						0x18
 
-#define NOP							0x00000000
-#define MAKE_CALL(f)				(0x0C000000 | (((u32)(f) >> 2) & 0x03ffffff))
+#define NOP										0x00000000
+#define MAKE_CALL(f)							(0x0C000000 | (((u32)(f) >> 2) & 0x03ffffff))
+
+#define PSP_SYSTEMPARAM_ID_INT_LANGUAGE         8
 
 PSP_MODULE_INFO("LangSwapper", PSP_MODULE_KERNEL, 1, 0);
 
 SceUID thid;
 u32 _sceImposeSetLanguageMode;
 u32 _sceUtilitySavedataInitStart, sd_sub;
+int ret, value;
 
 /**
  * Clears the data and instruction cache.
@@ -65,7 +70,7 @@ void patched_sceUtilitySavedataInitStart(u32 a0, u32 a1) {
 	// patches the final values for language/button.
 	for (i = 0; i < 2; i++) {
 		param_struct[i] = _lw(ptr[i]);
-		_sw(1, param_struct[i]);
+		_sw(value, param_struct[i]);
 	}
 
 	pspSdkSetK1(k1);
@@ -134,12 +139,19 @@ int mainThread(SceSize args, void *argp) {
 		patchHomeMenu(_sceImposeSetLanguageMode);
 	}
 
+	// Get the system language beforehand. Calling it elsewhere is gonna cause you to have bad time.
+	ret = sceUtilityGetSystemParamInt(PSP_SYSTEMPARAM_ID_INT_LANGUAGE, &value);
+	if(ret != 0) {
+		goto exit;
+	}
+
 	// Find the function in kernel land responsible for handling savedata.
 	_sceUtilitySavedataInitStart = sctrlHENFindFunction("sceUtility_Driver", "sceUtility", 0x50C4CD57);
 	if(_sceUtilitySavedataInitStart) {
 		patchSaveData(_sceUtilitySavedataInitStart);
 	}
 
+	exit:
 	ClearCaches();
 	return 0;
 }
